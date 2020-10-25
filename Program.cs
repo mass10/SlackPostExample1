e3736de575da3c9332338f7aaf6c9f58c38447dd
Irisawa Masaru <mass10.github@gmail.com>
2020-10-25 15:23:17 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,27 +6,26 @@ namespace SlackPostExample1
 	{
 		static void Main(string[] args)
 		{
+			// コンフィギュレーション
 			var conf = new Configuration();
 			conf.Configure();
 
-			// 投稿先チャネル
-			var channel = "notifications";
-			// メッセージ
-			var text = "これをさずけよう";
-			// ファイルのパス
-			var path = "IMG_3995.JPG";
-			// トークン(※Slack Application のページから出力されたものを使用します)
-			var slackAccessToken = conf.GetString("slack_accesskey");
-
 			var client = new SimpleClient();
 
+			// トークン(※Slack Application のページから出力されたものを使用します)
+			var slackAccessToken = conf.GetString("slack_accesskey");
 			client.AddHeader("Authorization", "Bearer " + slackAccessToken);
 
-			client.AddFormData("initial_comment", text);
-			client.AddFormData("channels", channel);
-			
-			client.AddFilePart("file", path);
+			// 投稿先チャネル
+			client.AddFormData("channels", "notifications");
 
+			// メッセージ
+			client.AddFormData("initial_comment", "これをさずけよう");
+
+			// 投稿するファイル
+			client.AddFilePart("file", "IMG_3995.JPG");
+
+			// リクエスト送出
 			var response = client.Post("https://slack.com/api/files.upload");
 
 			Console.WriteLine(response);
