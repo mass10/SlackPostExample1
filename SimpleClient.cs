72d6903dffe9238a49215c1e845347225cb0ccbb
Irisawa Masaru <mass10.github@gmail.com>
2020-10-25 15:18:47 +0900

    Update SimpleClient.cs

--- SimpleClient.cs
+++ SimpleClient.cs
@@ -4,7 +4,82 @@ using System.Text;
 
 namespace SlackPostExample1
 {
-	class SimpleClient
+	/// <summary>
+	/// 簡易 HTTP クライアントです。
+	/// </summary>
+	internal sealed class SimpleClient
 	{
+		/// <summary>
+		/// HTTP クライアント
+		/// </summary>
+		private System.Net.Http.HttpClient _client = null;
+
+		/// <summary>
+		/// フォームデータ
+		/// </summary>
+		private System.Net.Http.MultipartFormDataContent _form = null;
+
+		/// <summary>
+		/// コンストラクター
+		/// </summary>
+		public SimpleClient()
+		{
+			// 「SSL/TLS のセキュリティで保護されているチャネルを作成できませんでした」の回避
+			System.Net.ServicePointManager.SecurityProtocol =
+				System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12;
+
+			this._client = new System.Net.Http.HttpClient();
+
+			this._form = new System.Net.Http.MultipartFormDataContent();
+		}
+
+		/// <summary>
+		/// リクエストにヘッダー情報を追加します。
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="value"></param>
+		public void AddHeader(string name, string value)
+		{
+			this._client.DefaultRequestHeaders.Add(name, value);
+		}
+
+		/// <summary>
+		/// リクエストにフォームデータを追加します。
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="value"></param>
+		public void AddFormData(string name, string value)
+		{
+			var field = new System.Net.Http.StringContent(value);
+			this._form.Add(field, name);
+		}
+
+		/// <summary>
+		/// リクエストにファイルデータを追加します。
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="path"></param>
+		public void AddFilePart(string name, string path)
+		{
+			var attachment = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data");
+			attachment.Name = name;
+			attachment.FileName = System.IO.Path.GetFileName(path);
+
+			var fileContent = new System.Net.Http.StreamContent(System.IO.File.OpenRead(path));
+			fileContent.Headers.ContentDisposition = attachment;
+
+			this._form.Add(fileContent);
+		}
+
+		/// <summary>
+		/// POST
+		/// </summary>
+		/// <param name="url">URL</param>
+		/// <returns></returns>
+		public string Post(string url)
+		{
+			var response = this._client.PostAsync(url, this._form).Result;
+			return response.Content.ReadAsStringAsync().Result;
+		}
 	}
 }
